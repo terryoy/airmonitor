fbdb8ecc3f7c568b533032240b32dc4ac973ff85
terryoy <terry.ouyang@gmail.com>
2015-11-08 13:21:49 +0800

    add prefix to enums

--- air_v1/AirQualitySensor/AirModule.h
+++ air_v1/AirQualitySensor/AirModule.h
@@ -8,16 +8,16 @@
 
 /* Request data definition */
 enum Command {
-  QUERY = 0x01,
-  CLEAR = 0x02,
-  SPAN = 0x03,
-  FACTORY = 0x04,
+  CMD_QUERY = 0x01,
+  CMD_CLEAR = 0x02,
+  CMD_SPAN = 0x03,
+  CMD_FACTORY = 0x04,
 };
 
 typedef struct {
   char begin_1 = 0x42;
   char begin_2 = 0x4d;
-  char cmd = QUERY;
+  char cmd = CMD_QUERY;
   char dhh = 0;
   char dll = 0;
   char lrch = 0;
@@ -28,54 +28,54 @@ typedef struct {
 /* Response data definition */
 
 enum AirType {
-  NO_SENSOR = 0x00,
-  CO = 0x01,
-  H2S = 0x02, 
-  CH4 = 0x03,
-  CL2 = 0x04,
-  HCL = 0x05,
-  F2 = 0x06,
-  HF = 0x07,
-  NH3 = 0x08,
-  HCN = 0x09,
-  PH3 = 0x0a,
-  NO = 0x0b,
-  NO2 = 0x0c, 
-  O3 = 0x0d, 
-  O2 = 0x0e,
-  SO2 = 0x0f,
-  CLO2 = 0x10,
-  COCL2 = 0x11,
-  PH3_ = 0x12, // duplicated with 0x0a
-  SIH4 = 0x13,
-  HCHO = 0x14,
-  CO2 = 0x15,
-  VOC = 0x16,
-  ETO = 0x17,
-  C2H4 = 0x18,
-  C2H2 = 0x19,
-  SF6 = 0x1a,
-  ASH3 = 0x1b,
-  H2 = 0x1c,
-  TOX1 = 0x1d,
-  TOX2 = 0x1e,
-  GAS_FLOW = 0x1f, // L/M
-  BATTERY = 0x20,
+  AIRTYPE_NO_SENSOR = 0x00,
+  AIRTYPE_CO = 0x01,
+  AIRTYPE_H2S = 0x02, 
+  AIRTYPE_CH4 = 0x03,
+  AIRTYPE_CL2 = 0x04,
+  AIRTYPE_HCL = 0x05,
+  AIRTYPE_F2 = 0x06,
+  AIRTYPE_HF = 0x07,
+  AIRTYPE_NH3 = 0x08,
+  AIRTYPE_HCN = 0x09,
+  AIRTYPE_PH3 = 0x0a,
+  AIRTYPE_NO = 0x0b,
+  AIRTYPE_NO2 = 0x0c, 
+  AIRTYPE_O3 = 0x0d, 
+  AIRTYPE_O2 = 0x0e,
+  AIRTYPE_SO2 = 0x0f,
+  AIRTYPE_CLO2 = 0x10,
+  AIRTYPE_COCL2 = 0x11,
+  AIRTYPE_PH3_ = 0x12, // duplicated with 0x0a
+  AIRTYPE_SIH4 = 0x13,
+  AIRTYPE_HCHO = 0x14,
+  AIRTYPE_CO2 = 0x15,
+  AIRTYPE_VOC = 0x16,
+  AIRTYPE_ETO = 0x17,
+  AIRTYPE_C2H4 = 0x18,
+  AIRTYPE_C2H2 = 0x19,
+  AIRTYPE_SF6 = 0x1a,
+  AIRTYPE_ASH3 = 0x1b,
+  AIRTYPE_H2 = 0x1c,
+  AIRTYPE_TOX1 = 0x1d,
+  AIRTYPE_TOX2 = 0x1e,
+  AIRTYPE_GAS_FLOW = 0x1f, // L/M
+  AIRTYPE_BATTERY = 0x20,
 };
 
 enum MeasureUnit {
-  PPM = 0x01,
-  VOL = 0x02,
-  LEL = 0x03,
-  PPB = 0x04,
-  MG_M3 = 0x05,
+  MU_PPM = 0x01,
+  MU_VOL = 0x02,
+  MU_LEL = 0x03,
+  MU_PPB = 0x04,
+  MU_MG_M3 = 0x05,
 };
 
 enum Equivalent {
-  ONE = 0x01,
-  TEN = 0x02,
-  HUNDRED = 0x03,
-  THOUSAND = 0x04,
+  EQV_ONE = 0x01,
+  EQV_TEN = 0x02,
+  EQV_HUNDRED = 0x03,
+  EQV_THOUSAND = 0x04,
 };
 
 typedef struct {
@@ -106,15 +106,15 @@ void parse_air_response(AirResponse *response, char *raw) {
 
 char* get_unit_display(AirResponse response) {
   switch(response.unit) {
-    case PPM:
+    case MU_PPM:
       return "ppm";
-    case VOL:
+    case MU_VOL:
       return "VOL";
-    case LEL:
+    case MU_LEL:
       return "LEL";
-    case PPB:
+    case MU_PPB:
       return "Ppb";
-    case MG_M3:
+    case MU_MG_M3:
       return "Mg/m3";
     default:
       return "";
